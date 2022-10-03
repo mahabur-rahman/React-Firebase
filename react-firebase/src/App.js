bd00407583a6b7f34913fb0a15cd4b72b0ca19f9
mahabur  rahman <annur4395@gmail.com>
2022-10-04 05:07:51 +0600

    firebase query with fireStore

--- react-firebase/src/App.js
+++ react-firebase/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import FirebaseQueries from "./components/FirebaseQueries";
 import FireRealTime from "./components/FireRealTime";
 import FireStorage from "./components/FireStorage";
 import FireStore from "./components/FireStore";
 import FormData from "./components/FormData";
 
 const App = () => {
-  return <FireRealTime />;
+  return <FirebaseQueries />;
 };
 
 export default App;
