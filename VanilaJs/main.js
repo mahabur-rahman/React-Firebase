e89a9dae4ad208890188a3b6f3a7ddd3e6affbc7
mahabur  rahman <annur4395@gmail.com>
2022-10-30 19:44:52 +0600

    crud operation with vanilaJs + firebase store

--- VanilaJs/main.js
+++ VanilaJs/main.js
@@ -26,3 +26,81 @@ const register = () => {
       console.log(err.message);
     });
 };
+
+// login
+const login = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((res) => {
+      console.log(res.user);
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err.code);
+      console.log(err.message);
+    });
+};
+
+// add data
+const addData = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  db.collection("users")
+    .add({
+      password,
+      email,
+    })
+    .then((res) => {
+      console.log(res.id);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+// read data
+const readData = () => {
+  db.collection("users")
+    .get()
+    .then((data) => {
+      console.log(
+        data.docs.map((item) => {
+          return { ...item.data(), id: item.id };
+        })
+      );
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+// update data
+
+const updateData = () => {
+  db.collection("users", doc(`GeexGYnU51GfhCE9Tz0F`))
+    .update({
+      email: "noman@gmail.com",
+      password: "123456",
+    })
+    .then(() => {
+      alert("data updated!");
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+// delete data
+const deleteData = () => {
+  db.collection("users", doc(`nKBRZFT014iBbG4MOEFV`).delete())
+    .then(() => {
+      alert("deleted..");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
